af8bb46ba332a65b4fbbdb711a4717e3727618a5
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-08-22 15:18:20 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,16 +11,24 @@ form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-  for (let position = 1; 1 < amountEl.value; position += 1) {
-    const delay = (position - 1) * stepEl.value + +delayEl.value;
+  let delay = 0;
+  for (let i = 1; i < amountEl.value; i++) {
+    const position = i;
+    if (position === 1) {
+      delay += delayEl.value;
+    } else {
+      delay += stepEl.value;
+    }
+    // const delay = (position - 1) * stepEl.value + +delayEl.value;
+
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
       .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
   }
 }
