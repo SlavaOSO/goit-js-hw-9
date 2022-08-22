55428b771be9e91954e800c9a9e11dee7529e139
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-08-22 15:37:14 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,59 +1,97 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const form = document.querySelector('.form');
 const delayEl = document.querySelector('[name="delay"]');
 const stepEl = document.querySelector('[name="step"]');
 const amountEl = document.querySelector('[name="amount"]');
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', onSubmit);
+form.addEventListener('submit', (onSubmitForm));
 
+let position = 0;
 
+function onSubmitForm(e){
+e.preventDefault();
+let delayStep = delayEl.value ;
+const step = stepEl.value;
+const amount = amountEl.value;
 
-function onSubmit(e) {
-  e.preventDefault();
-  const firstDelay = delayEl.value;
-  const stepDelay = stepEl.value;
-  const amountForm = amountEl.value;
-  let delay = 0;
-  for (let i = 1; i < amountForm; i++) {
-    const position = i;
-    if (position === 1) {
-      delay += firstDelay;
-    } else {
-      delay += stepDelay;
-    }
-    // const delay = (position - 1) * stepEl.value + +delayEl.value;
-
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-  }
+for (position = 1; position <= amount; position += 1) {
+  const delay = (position - 1) * step + +delayStep;
+createPromise(position, delay)
+.then(({ position, delay }) => Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+  .catch(({ position, delay }) => Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
 }
-
+}
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-    
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay })
-        // Fulfill
-      } else {
-        reject({ position, delay })
-        // Reject
-      }
-    }, delay);
-  });
-      
+  setTimeout(() => {
+    if(shouldResolve){
+    resolve({position, delay})
+    } else {
+    reject({position, delay})
+  }
+  }, delay);
+});
 }
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// const form = document.querySelector('.form');
+// const delayEl = document.querySelector('[name="delay"]');
+// const stepEl = document.querySelector('[name="step"]');
+// const amountEl = document.querySelector('[name="amount"]');
+
+// form.addEventListener('submit', onSubmit);
+
+
+
+// function onSubmit(e) {
+//   e.preventDefault();
+//   const firstDelay = delayEl.value;
+//   const stepDelay = stepEl.value;
+//   const amountForm = amountEl.value;
+//   let delay = 0;
+//   for (let i = 1; i < amountForm; i++) {
+//     const position = i;
+//     if (position === 1) {
+//       delay += firstDelay;
+//     } else {
+//       delay += stepDelay;
+//     }
+//     // const delay = (position - 1) * stepEl.value + +delayEl.value;
+
+//     createPromise(position, delay)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+//   }
+// }
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   return new Promise((resolve, reject) => {
+    
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay })
+//         // Fulfill
+//       } else {
+//         reject({ position, delay })
+//         // Reject
+//       }
+//     }, delay);
+//   });
+      
+// }
 
 
 
