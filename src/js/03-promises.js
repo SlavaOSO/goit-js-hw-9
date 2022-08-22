1946b3985d26d59e6159c4baa683f18aac7464b0
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-08-22 16:24:25 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -65,14 +65,14 @@ function onSubmit(e) {
     // const delay = (position - 1) * stepEl.value + +delayEl.value;
 
     createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
         // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-      .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
         // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  );
   }
 }
 
